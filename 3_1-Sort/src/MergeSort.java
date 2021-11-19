e4a9b0e4c4de2d323124ae13c8181ebe25751957
ian <38461291+stutterrrrer@users.noreply.github.com>
2021-11-19 20:21:55 +0800

    MergeSort.java completed

--- 3_1-Sort/src/MergeSort.java
+++ 3_1-Sort/src/MergeSort.java
@@ -1,6 +1,29 @@
 public class MergeSort {
 
-	private static void merge(Comparable[] arr, Comparable[] auxiliaryArr,
+	public static void mergeSort(Comparable[] arr) {
+		int n = arr.length;
+		// make sure to create only 1 auxiliary array in this public method
+		// otherwise if the auxiliary array is created in the sort() method,
+			// since it's recursive, #of auxArr created would be = #of times sort() is called
+		Comparable[] aux = new Comparable[n];
+		sort(arr, aux, 0, n - 1);
+	}
+
+	// recursive sort - sorts left, then sorts right, then merge 2 sorted halves.
+	private static void sort(Comparable[] arr, Comparable[] aux, int left, int right) {
+		// base case:
+		// when the arr segment we are trying to sort is only 1 element -> already "sorted".
+		if (right <= left) return;
+
+		int mid = left + (right - left) / 2;
+		sort(arr, aux, left, mid);
+		sort(arr, aux, mid + 1, right);
+
+		merge(arr, aux, left, mid, right);
+	}
+
+	// merge - the method that does the heavy lifting.
+	private static void merge(Comparable[] arr, Comparable[] aux,
 							  int left, int mid, int right) {
 		// precondition: both halves are already sorted.
 		// left half: sorted from arr[left] to arr[mid].
@@ -8,7 +31,7 @@ public class MergeSort {
 		assert isSorted(arr, mid + 1, right);
 
 		// copy entire array to auxiliary array:
-		for (int j = left; j <= right; j++) auxiliaryArr[j] = arr[j];
+		for (int j = left; j <= right; j++) aux[j] = arr[j];
 
 		// merge left and right in sorted order:
 		// i.e. copy the sorted result of auxiliary array to original array:
@@ -18,23 +41,24 @@ public class MergeSort {
 			// compare the first of left remaining with first of right remaining:
 			if (leftNext <= mid && rightNext <= right) {
 				arr[arrNext] =
-						less(auxiliaryArr[leftNext], auxiliaryArr[rightNext]) ?
-								auxiliaryArr[leftNext++] :
-								auxiliaryArr[rightNext++];
+						less(aux[leftNext], aux[rightNext]) ?
+								aux[leftNext++] :
+								aux[rightNext++];
 			}
 
-			else {// either the left or the right half has all been copied into original arr:
+			else { // either the left or the right half has all been copied into original arr:
 				arr[arrNext] = leftNext > mid ?
 						// if it's the left half:
 						// copy the rest of the auxArr's right half into original array as is
-						auxiliaryArr[rightNext++] :
-						auxiliaryArr[leftNext++];
+						aux[rightNext++] :
+						aux[leftNext++];
 			}
 		}
 
 		// after the merge - original arr should now be sorted from left to right.
 		assert isSorted(arr, left, right);
 	}
+
 
 	private static boolean isSorted(Comparable[] arr, int left, int right) {
 		for (int i = left + 1; i <= right; i++)
@@ -45,5 +69,12 @@ public class MergeSort {
 
 	private static boolean less(Comparable v, Comparable w) {
 		return v.compareTo(w) < 0;
+	}
+
+	public static void main(String[] args) {
+		Integer[] arr = {3, 5, 1, 4, 0, -5, -6, -12, 23, -50};
+		MergeSort.mergeSort(arr);
+		for (int i : arr)
+			System.out.print(i + ", ");
 	}
 }
