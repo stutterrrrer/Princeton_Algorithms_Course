3530e216acd94ceded6cd95a34898e103c08985e
ian <38461291+stutterrrrer@users.noreply.github.com>
2021-12-31 00:10:01 +0800

    revised digraph implementation

--- 6_3-WordNet_assignment/WordNet.java
+++ 6_3-WordNet_assignment/WordNet.java
@@ -1,21 +1,15 @@
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.DirectedCycle;
 import edu.princeton.cs.algs4.In;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.TreeMap;
 
 public class WordNet {
     private final int synsetCount;
-    private int root;
-    //TODO use alg4 Digraph - SAP's constructor parameter
-    private LinkedList<Integer>[] adjHyper;
-    private LinkedList<Integer>[] adjHypo;
+    private Digraph toHyper;
+    private Digraph toHypo; // need the reverse digraph for ancestral path?
     private TreeMap<String, List<Integer>> nounIDs;
-
-    // public API doesn't allow returning adjacent lists - no Cycle class possible
-    private boolean foundCycle;
-    private int visitedCount;
 
     public WordNet(String synsets, String hypernyms) {
         if (synsets == null || hypernyms == null) throw new IllegalArgumentException();
@@ -27,20 +21,15 @@ public class WordNet {
         // file-reading is not done with a try-with block, need to close manually
         fileReader.close();
 
+        toHyper = new Digraph(synsetCount);
         // since key size is unknown, hashmap may need too many resizing
         nounIDs = new TreeMap<>();
-        adjHyper = (LinkedList<Integer>[]) new Object[synsetCount];
-        adjHypo = (LinkedList<Integer>[]) new Object[synsetCount];
-        for (int i = 0; i < synsetCount; i++) {
-            adjHyper[i] = new LinkedList<>();
-            adjHypo[i] = new LinkedList<>();
-        }
 
-        loadIntoAdjLists(hypernyms);
+        loadIntoDigraph(hypernyms);
         populateNounIDs(synsets);
     }
 
-    private void loadIntoAdjLists(String hypernyms) {
+    private void loadIntoDigraph(String hypernyms) {
         int rootCount = 0;
         In fileReader = new In(hypernyms);
         while (fileReader.hasNextLine()) {
@@ -48,34 +37,18 @@ public class WordNet {
             if (splits[0].isEmpty()) continue;
 
             int hypo = Integer.parseInt(splits[0]);
-            if (splits.length == 1) {
-                root = hypo;
-                rootCount++;
-            }
+            if (splits.length == 1) rootCount++;
 
             for (int i = 1; i < splits.length; i++) {
                 int hyper = Integer.parseInt(splits[i]);
-                adjHyper[hypo].add(hyper);
-                adjHypo[hyper].add(hypo);
+                toHyper.addEdge(hypo, hyper);
             }
         }
-        if (rootCount != 1 || containsCycle()) throw new IllegalArgumentException();
-    }
 
-    private boolean containsCycle() {
-        boolean[] visited = new boolean[synsetCount];
-        boolean[] prevOnThisPath = new boolean[synsetCount];
-        // if there's only 1 root,
-        // then in a digraph pointing from root to hyponyms,
-        // all vertices are reachable from root unless there's a cycle
-        dfsForCycle(root, visited, prevOnThisPath);
-    }
+        if (rootCount != 1 || new DirectedCycle(toHyper).hasCycle())
+            throw new IllegalArgumentException();
 
-    private void dfsForCycle(int v, boolean[] visited, boolean[] prevOnThisPath) {
-        visited[v] = true;
-        prevOnThisPath[v] = true;
-        for (int w : adjHyper[v])
-            if (!visited[w])
+        toHypo = toHyper.reverse();
     }
 
     private void populateNounIDs(String synsets) {
